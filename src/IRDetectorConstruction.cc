7d80de6bee938f0cfbe875cd6701fd6af5b6d5e6
Andrey <kenjo@jlab.org>
2021-03-16 17:49:47 -0400

    geometry update

--- src/IRDetectorConstruction.cc
+++ src/IRDetectorConstruction.cc
@@ -41,7 +41,6 @@ G4VPhysicalVolume* IRDetectorConstruction::Construct()
 
   G4double world_sizeXY = 3*m;
   G4double world_sizeZ  = 120*m;
-  G4Material* world_mat = nist->FindOrBuildMaterial("G4_Galactic");
 
   double rr = 8.31446261815324;
   double roomtemp = 293*kelvin;
@@ -68,248 +67,44 @@ G4VPhysicalVolume* IRDetectorConstruction::Construct()
   G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "World", 0, false, 0, checkOverlaps);
 
 
-  G4RotationMatrix *stlrm = new G4RotationMatrix();
-  stlrm->rotateY(90*degree);
-  stlrm->rotateX(90*degree);
+  std::string vols[][2] = {
+    {"stl/Detector_chamber_-_beryllium_section.stl", "G4_Be"},			// 1
+    {"stl/Detector_chamber_-_electron_exit_round.stl", "G4_W"},			// 2
+    {"stl/Detector_chamber_-_hadron_entrance_round.stl", "G4_Al"},		// 3
+    {"stl/Detector_chamber_-_hadron_forward.stl", "G4_W"},				// 4
+    {"stl/Detector_chamber_-_inner_hadron_forward.stl", "G4_Al"},		// 5
+    {"stl/Detector_chamber_-_inner_hadron_rear_round.stl", "G4_Al"},		// 6
+    {"stl/DN120_CF_blank.stl", "G4_STAINLESS-STEEL"},					// 7
+    {"stl/DN120_CF_blank001.stl", "G4_STAINLESS-STEEL"},				// 8
+    {"stl/DN50_hadron_rear_flange.stl", "G4_STAINLESS-STEEL"},			// 9
+    {"stl/PI-FLG-0061.stl", "G4_STAINLESS-STEEL"},					// 10
+    {"stl/PI-FLG-0062-1.stl", "G4_STAINLESS-STEEL"},					// 11
+    //{"stl/PI-FLG-0062-2.stl", "G4_STAINLESS-STEEL"},
+    {"stl/PI-FLG-0068-01.stl", "G4_STAINLESS-STEEL"},					// 12
+    {"stl/PI-FLG-0068-02.stl", "G4_STAINLESS-STEEL"},					// 13
+    {"stl/PI-FLG-0400.stl", "G4_STAINLESS-STEEL"},					// 14
+    {"stl/upstream_0.stl", "G4_W"},								// 15
+    {"stl/upstream_1.stl", "G4_Al"},								// 16
+    {"stl/upstream_2.stl", "G4_Al"}								// 17
+  };
+
+  int ivol = 0;
+  for(auto vol: vols) {
+    ivol++;
+
+    char vname[100];
+    sprintf(vname, "vol%02d", ivol);
+    std::string vstr(vname);
+
+    auto vol_mesh = CADMesh::TessellatedMesh::FromSTL(vol[0]);
+    auto vol_sol = vol_mesh->GetSolid();
+    auto vol_log = new G4LogicalVolume(vol_sol, nist->FindOrBuildMaterial(vol[1]), vstr+"_log", 0, 0, 0);
+    auto vol_phy = new G4PVPlacement(0, G4ThreeVector(), vol_log, vstr+"_phy", logicWorld, false, 0, checkOverlaps);
+
+    if(ivol==1)
+      sensitives[vol_phy] = 1;
+  }
 
-
-//Central Chamber - in future need 2um G4_Au coating
-//  CADMesh * vol01_mesh = new CADMesh("stl/Detectorchamber_Be_section.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  //auto vol01_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_Be_section.stl.stl");
-  auto vol01_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_Be_section.stl.stl");
-  G4VSolid* vol01_solid = vol01_mesh->GetSolid();
-  G4LogicalVolume* vol01_logic = new G4LogicalVolume(vol01_solid, nist->FindOrBuildMaterial("G4_Be"), "vol01_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol01_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol01_logic, "vol01_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//Description D/S ion chamber, aluminum
-//  CADMesh * vol02_mesh = new CADMesh("stl/Detectorchamber_inner_hadron_forward.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol02_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_inner_hadron_forward.stl.stl");
-  G4VSolid* vol02_solid = vol02_mesh->GetSolid();
-  G4LogicalVolume* vol02_logic = new G4LogicalVolume(vol02_solid, nist->FindOrBuildMaterial("G4_Al"), "vol02_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol02_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol02_logic, "vol02_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//Electron beam  screen inside d/s ion inner chamber
-//  CADMesh * vol03_mesh = new CADMesh("stl/Electron_beamscreen_inner_chamber.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol03_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Electron_beamscreen_inner_chamber.stl.stl");
-  G4VSolid* vol03_solid = vol03_mesh->GetSolid();
-  G4LogicalVolume* vol03_logic = new G4LogicalVolume(vol03_solid, nist->FindOrBuildMaterial("G4_W"), "vol03_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol03_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol03_logic, "vol03_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//Inner flange, hadron forward
-//  CADMesh * vol04_mesh = new CADMesh("stl/PI-FLG-0061.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol04_mesh = CADMesh::TessellatedMesh::FromSTL("stl/PI-FLG-0061.stl.stl");
-  G4VSolid* vol04_solid = vol04_mesh->GetSolid();
-  G4LogicalVolume* vol04_logic = new G4LogicalVolume(vol04_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol04_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol04_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol04_logic, "vol04_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//Ion beam pipe in hadron forward
-  G4RotationMatrix *ionrm = new G4RotationMatrix();
-  ionrm->rotateY(180*degree);
-  auto vol000_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Upstream_beampipe.stl.stl");
-  G4VSolid* vol000_solid = vol000_mesh->GetSolid();
-  G4LogicalVolume* vol000_logic = new G4LogicalVolume(vol000_solid, nist->FindOrBuildMaterial("G4_Al"), "vol000_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol000_phys = new G4PVPlacement(ionrm, G4ThreeVector(69.9066, -52.4124, 37800), vol000_logic, "vol000_phys", logicWorld, false, 0, checkOverlaps);
-
-//Ion beam pipe in hadron forward
-//  CADMesh * vol05_mesh = new CADMesh("stl/Detectorchamber_hadron_forward.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol05_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_hadron_forward.stl.stl");
-  G4VSolid* vol05_solid = vol05_mesh->GetSolid();
-  G4LogicalVolume* vol05_logic = new G4LogicalVolume(vol05_solid, nist->FindOrBuildMaterial("G4_Al"), "vol05_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol05_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol05_logic, "vol05_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//rotatable inner flange -  hadron forward
-//  CADMesh * vol06_mesh = new CADMesh("stl/PI-FLG-0062-1.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol06_mesh = CADMesh::TessellatedMesh::FromSTL("stl/PI-FLG-0062-1.stl.stl");
-  G4VSolid* vol06_solid = vol06_mesh->GetSolid();
-  G4LogicalVolume* vol06_logic = new G4LogicalVolume(vol06_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol06_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol06_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol06_logic, "vol06_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//rotatable inner flange, hadron forward, fits inside PI-FLG-0062-1
-//  CADMesh * vol07_mesh = new CADMesh("stl/PI-FLG-0062-2.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol07_mesh = CADMesh::TessellatedMesh::FromSTL("stl/PI-FLG-0062-2.stl.stl");
-  G4VSolid* vol07_solid = vol07_mesh->GetSolid();
-  G4LogicalVolume* vol07_logic = new G4LogicalVolume(vol07_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol07_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol07_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol07_logic, "vol07_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//flange on port to Detector Chamber - hadron forward, also location of NEG #301 in hadron forward
-//  CADMesh * vol08_mesh = new CADMesh("stl/DN63_CF.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol08_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF.stl.stl");
-  G4VSolid* vol08_solid = vol08_mesh->GetSolid();
-  G4LogicalVolume* vol08_logic = new G4LogicalVolume(vol08_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol08_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol08_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol08_logic, "vol08_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//flange on port to Detector Chamber
-//  CADMesh * vol09_mesh = new CADMesh("stl/DN63_CF001.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol09_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF001.stl.stl");
-  G4VSolid* vol09_solid = vol09_mesh->GetSolid();
-  G4LogicalVolume* vol09_logic = new G4LogicalVolume(vol09_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol09_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol09_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol09_logic, "vol09_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//flange on port to Detector Chamber
-//  CADMesh * vol10_mesh = new CADMesh("stl/DN63_CF002.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol10_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF002.stl.stl");
-  G4VSolid* vol10_solid = vol10_mesh->GetSolid();
-  G4LogicalVolume* vol10_logic = new G4LogicalVolume(vol10_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol10_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol10_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol10_logic, "vol10_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//flange on port to Detector Chamber
-//  CADMesh * vol11_mesh = new CADMesh("stl/DN63_CF003.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol11_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF003.stl.stl");
-  G4VSolid* vol11_solid = vol11_mesh->GetSolid();
-  G4LogicalVolume* vol11_logic = new G4LogicalVolume(vol11_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol11_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol11_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol11_logic, "vol11_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//Outer flange - hadron forward detector chamber
-//  CADMesh * vol12_mesh = new CADMesh("stl/DN250_CF_fixed_blank.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol12_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN250_CF_fixed_blank.stl.stl");
-  G4VSolid* vol12_solid = vol12_mesh->GetSolid();
-  G4LogicalVolume* vol12_logic = new G4LogicalVolume(vol12_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol12_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol12_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol12_logic, "vol12_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//Hadron forward electron beam screen
-//  CADMesh * vol13_mesh = new CADMesh("stl/Electron_beamscreen_SOLID.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol13_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Electron_beamscreen_SOLID.stl.stl");
-  G4VSolid* vol13_solid = vol13_mesh->GetSolid();
-  G4LogicalVolume* vol13_logic = new G4LogicalVolume(vol13_solid, nist->FindOrBuildMaterial("G4_W"), "vol13_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol13_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol13_logic, "vol13_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-////Does not render in CAD. Electron beam screen
-////  CADMesh * vol14_mesh = new CADMesh("stl/Electron_beamscreen_COMPOUND.STL .stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-//  auto vol14_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Electron_beamscreen_COMPOUND.STL.stl.stl");
-//  G4VSolid* vol14_solid = vol14_mesh->GetSolid();
-//  G4LogicalVolume* vol14_logic = new G4LogicalVolume(vol14_solid, nist->FindOrBuildMaterial("G4_W"), "vol14_logic", 0, 0, 0);
-//  G4VPhysicalVolume* vol14_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol14_logic, "vol14_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//hadron rear/ele forward chamber rectangular, 1st half
-//  CADMesh * vol15_mesh = new CADMesh("stl/Detectorchamber_inner_hadron_rear_SOLID001.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol15_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_inner_hadron_rear_SOLID001.stl.stl");
-  G4VSolid* vol15_solid = vol15_mesh->GetSolid();
-  G4LogicalVolume* vol15_logic = new G4LogicalVolume(vol15_solid, nist->FindOrBuildMaterial("G4_Al"), "vol15_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol15_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol15_logic, "vol15_phys", logicWorld, false, 0, checkOverlaps);
-
-
-////hadron rear/ele forward chamber rectangular, DOES NOT RENDER
-////  CADMesh * vol16_mesh = new CADMesh("stl/Detectorchamber_inner_hadron_rear_COMPOUND001.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-//  auto vol16_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_inner_hadron_rear_COMPOUND001.stl.stl");
-//  G4VSolid* vol16_solid = vol16_mesh->GetSolid();
-//  G4LogicalVolume* vol16_logic = new G4LogicalVolume(vol16_solid, nist->FindOrBuildMaterial("G4_Al"), "vol16_logic", 0, 0, 0);
-//  G4VPhysicalVolume* vol16_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol16_logic, "vol16_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//Inner flange hadron rear between sections of ele forward chamber
-//  CADMesh * vol17_mesh = new CADMesh("stl/DN120_CF_blank.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol17_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN120_CF_blank.stl.stl");
-  G4VSolid* vol17_solid = vol17_mesh->GetSolid();
-  G4LogicalVolume* vol17_logic = new G4LogicalVolume(vol17_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol17_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol17_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol17_logic, "vol17_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//Detector chamber - hadron rear/ele forward, rectangular, 2nd half + integrated pumping chamber
-//  CADMesh * vol18_mesh = new CADMesh("stl/Detectorchamber_hadron_rear.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol18_mesh = CADMesh::TessellatedMesh::FromSTL("stl/Detectorchamber_hadron_rear.stl.stl");
-  G4VSolid* vol18_solid = vol18_mesh->GetSolid();
-  G4LogicalVolume* vol18_logic = new G4LogicalVolume(vol18_solid, nist->FindOrBuildMaterial("G4_Al"), "vol18_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol18_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol18_logic, "vol18_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//Inner flange hadron rear001 between sections of ele forward chamber, fits against DN120 CF blank
-//  CADMesh * vol19_mesh = new CADMesh("stl/DN120_CF_blank001.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol19_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN120_CF_blank001.stl.stl");
-  G4VSolid* vol19_solid = vol19_mesh->GetSolid();
-  G4LogicalVolume* vol19_logic = new G4LogicalVolume(vol19_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol19_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol19_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol19_logic, "vol19_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//flange on port of integrated pumping chamber on Detector chamber - hadron rear
-//  CADMesh * vol20_mesh = new CADMesh("stl/DN63_CF004.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol20_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF004.stl.stl");
-  G4VSolid* vol20_solid = vol20_mesh->GetSolid();
-  G4LogicalVolume* vol20_logic = new G4LogicalVolume(vol20_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol20_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol20_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol20_logic, "vol20_phys", logicWorld, false, 0, checkOverlaps);
-
-
-
-//flange on port of integrated pumping chamber on Detector chamber - hadron rear
-//  CADMesh * vol21_mesh = new CADMesh("stl/DN63_CF005.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol21_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF005.stl.stl");
-  G4VSolid* vol21_solid = vol21_mesh->GetSolid();
-  G4LogicalVolume* vol21_logic = new G4LogicalVolume(vol21_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol21_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol21_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol21_logic, "vol21_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//flange on port of integrated pumping chamber on Detector chamber - hadron rear
-//  CADMesh * vol22_mesh = new CADMesh("stl/DN63_CF006.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol22_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF006.stl.stl");
-  G4VSolid* vol22_solid = vol22_mesh->GetSolid();
-  G4LogicalVolume* vol22_logic = new G4LogicalVolume(vol22_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol22_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol22_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol22_logic, "vol22_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//flange on port of integrated pumping chamber on Detector chamber - hadron rear
-//  CADMesh * vol23_mesh = new CADMesh("stl/DN63_CF007.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol23_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN63_CF007.stl.stl");
-  G4VSolid* vol23_solid = vol23_mesh->GetSolid();
-  G4LogicalVolume* vol23_logic = new G4LogicalVolume(vol23_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol23_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol23_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol23_logic, "vol23_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//outer flange- hadron rear
-//  CADMesh * vol24_mesh = new CADMesh("stl/DN200_CF_rotatable_blank.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol24_mesh = CADMesh::TessellatedMesh::FromSTL("stl/DN200_CF_rotatable_blank.stl.stl");
-  G4VSolid* vol24_solid = vol24_mesh->GetSolid();
-  G4LogicalVolume* vol24_logic = new G4LogicalVolume(vol24_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol24_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol24_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol24_logic, "vol24_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//NEG Pump ion d/s
-//  CADMesh * vol26_mesh = new CADMesh("stl/5H0201NEXTorrZ_300.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol26_mesh = CADMesh::TessellatedMesh::FromSTL("stl/5H0201NEXTorrZ_300.stl.stl");
-  G4VSolid* vol26_solid = vol26_mesh->GetSolid();
-  G4LogicalVolume* vol26_logic = new G4LogicalVolume(vol26_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol26_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol26_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol26_logic, "vol26_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//NEG Pump ele d/s
-//  CADMesh * vol27_mesh = new CADMesh("stl/5H0201NEXTorrZ_301.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol27_mesh = CADMesh::TessellatedMesh::FromSTL("stl/5H0201NEXTorrZ_301.stl.stl");
-  G4VSolid* vol27_solid = vol27_mesh->GetSolid();
-  G4LogicalVolume* vol27_logic = new G4LogicalVolume(vol27_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol27_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol27_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol27_logic, "vol27_phys", logicWorld, false, 0, checkOverlaps);
-
-
-//NEG Pump ele d/s
-//  CADMesh * vol28_mesh = new CADMesh("stl/5H0201NEXTorrZ_302.stl.stl", mm, G4ThreeVector(0, 0, 0), false);
-  auto vol28_mesh = CADMesh::TessellatedMesh::FromSTL("stl/5H0201NEXTorrZ_302.stl.stl");
-  G4VSolid* vol28_solid = vol28_mesh->GetSolid();
-  G4LogicalVolume* vol28_logic = new G4LogicalVolume(vol28_solid, nist->FindOrBuildMaterial("G4_STAINLESS-STEEL"), "vol28_logic", 0, 0, 0);
-  G4VPhysicalVolume* vol28_phys = new G4PVPlacement(stlrm, G4ThreeVector(), vol28_logic, "vol28_phys", logicWorld, false, 0, checkOverlaps);
-
-
-  sensitives[vol01_phys] = 1;
 
 /*
   sensitives[vol04_phys] = 4;
